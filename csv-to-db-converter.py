5a29e1d7b44b0c41dd672d34adbd277c5af5d75c
Anh Duy Hoang <hoangduyanh30@gmail.com>
2020-03-23 17:39:45 +0200

    Changed root database to JSON

--- csv-to-db-converter.py
+++ csv-to-db-converter.py
@@ -5,11 +5,11 @@ Created on Tue Mar  3 23:13:37 2020
 @author: ADMIN
 """
 import pandas as pd
-import sqlite3
+import json
+import numpy as np
 
-game_db = pd.read_csv('game_data_2.csv')
-conn = sqlite3.connect("game_data.db")
-game_db.to_sql('games', conn, if_exists='append', index=False)
-
-c = conn.cursor()
-c.execute
+df = pd.read_csv('game_data_2.csv')
+df = df.replace({np.nan:None})
+df= df.to_dict('records')
+with open('game_db.json', 'w') as game_data:
+    json.dump(df, game_data)
--- main.py
+++ main.py
@@ -2,191 +2,147 @@
 """
 Created on Mon Mar  2 11:14:07 2020
 
-@author: Anh Hoang
+@author: Anh Duy Hoang
 """
-#TODO:
-# fix some names
-# test post and delete METHOD
-# set ID to be primary key (I CAN'T YOU JUST HAVE TO REMEMBER THE IDs)
-# comments
-from flask import Flask, request, jsonify
+
+from flask import Flask, request
 from flask_restful import Resource, Api, reqparse, abort
-import sqlite3
+import json
 
 app = Flask(__name__)
 api = Api(app)
 app.config['JSON_SORT_KEYS'] = False
+
 parser = reqparse.RequestParser()
+parser.add_argument('name', 'platform', 'year', 'genre', 'publisher', 'NA_sales', 'EU_sales', 'JP_sales', 'other_sales',
+                    'global_sales', 'developer')
+with open('game_db.json', 'r') as game_data:
+    db = json.load(game_data)
 
 
-def dict_factory(cursor, row):
-    d = {}
-    for (idx, col) in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
+# Generate URL to Homepage
+# endpoint '/'
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Game Sale API</h1>"
 
 
-def convert_limit(limit):
-    query = 'SELECT Name, COUNT(id) as Total\
-            FROM games\
-            GROUP BY Name\
-            LIMIT ?;'
-    conn = sqlite3.connect('game_data.db')
-    cur = conn.cursor()
-    df = cur.execute(query,(limit,)).fetchall()
-    limit_param = 0
-    for row in df:
-        limit_param += row[1]
-    return limit_param
-
-
+# Generate the endpoint to the entire list of games
+# endpoint '/api/v1/games/'
 class GameList(Resource):
     def get(self):
-        query_params = request.args
-        query = "SELECT * FROM games "
+        args = request.args
 
-        limit = query_params.get('limit')
-        
-        conn = sqlite3.connect('game_data.db')
-        conn.row_factory = dict_factory
-        cur = conn.cursor()
-            
-        if limit:
-            query += "LIMIT ?;"
-            result = cur.execute(query, (limit,)).fetchall()
+        if args.get('limit'):
+            limit = int(args.get('limit'))
+            return db[:limit]
         else:
-            query += 'LIMIT 100;'
-            result = cur.execute(query).fetchall()
-        return jsonify(result)
+            return db[:20]
 
     def post(self):
-        parser.add_argument('ID', 'Name', 'Platform', 'Year_of_Release', 'Genre',
-                            'Publisher', 'NA_Sales', 'EU_Sales', 'JP_Sales',
-                            'Other_Sales', 'Global_Sales', 'Developer')
         args = parser.parse_args()
-        conn = sqlite3.connect('game_data.db')
-        cur = conn.cursor()
-        cur.execute('INSERT INTO games\
-                    VALUES (?,?,?,?,?,?,?,?,?,?,?,?)', args.values())
-        conn.commit()
-        conn.close()
-        return '', 201
+        item_id = int(db[-1]['ID']) + 1
+        d = {'ID': item_id,
+             'Name': args['name'],
+             'Platform': args['platform'],
+             'Year': args['year'],
+             'Genre': args['genre'],
+             'Publisher': args['publisher'],
+             'NA_Sales': args['NA_sales'],
+             'JP_Sales': args['JP_sales'],
+             'Other_Sales': args['other_sales'],
+             'Global_Sales': args['global_sales'],
+             'Developer': args['developer']}
+        db.append(d)
+        if len(d) != 11:
+            abort(400)
+        return d, 201
 
-    def delete(self):
-        item_id = request.args.get('id')
-        conn = sqlite3.connect('game_data.db')
-        cur = conn.cursor()
-        cur.execute('DELETE FROM games WHERE ID = ?;', item_id).fetchall()
-        conn.commit()
-        conn.close()
+
+# Generate endpoints to search for specific game and its data
+# endpoint '/api/v1/games/search/'
+class GameByParams(Resource):
+    def get(self, game_id):
+        result = [item for item in db if item['ID'] == game_id]
+        return result
+
+    def put(self, game_id):
+        args = parser.parse_args()
+        result = [item for item in db if item['ID'] == game_id][0]
+        result['Name'] = args['name']
+        result['Platform'] = args['platform']
+        result['Year'] = args['year']
+        result['Genre'] = args['genre']
+        result['Publisher'] = args['publisher']
+        result['NA_Sales'] = args['NA_sales']
+        result['JP_Sales'] = args['JP_sales']
+        result['Other_Sales'] = args['other_sales']
+        result['Global_Sales'] = args['global_sales']
+        result['Developer'] = args['developer']
+
+        return result, 200
+
+    def delete(self, game_id):
+        del db[game_id]
         return '', 204
 
 
-class GameByName(Resource):
-    def get(self):
-        query_params = request.args
-        query = 'SELECT * FROM games WHERE '
-
-        name = query_params.get('name')
-        sales = query_params.get('sales')
-        after = query_params.get('after')
-        limit = query_params.get('limit')
-
-        params_filter = []
-
-        conn = sqlite3.connect('game_data.db')
-        conn.row_factory = dict_factory
-        cur = conn.cursor()
-        if name:
-            query += 'name = ? AND'
-            params_filter.append(name)
-        if sales:
-            query += 'Global_Sales > ? AND'
-            params_filter.append(sales)
-        if after:
-            query += 'ID > ? AND'
-            params_filter.append(after)
-        query = query[:-3]
-
-        if limit:
-            query += "LIMIT ?;"
-            params_filter.append(limit)
-        else:
-            query += ';'
-        result = cur.execute(query, params_filter).fetchall()
-
-        if len(result) == 0:
-            abort(404)
-        return jsonify(result)
-
-
+# Generate endpoints to entire list of games and theirs sales across different platforms (if there's more than one)
+# endpoint '/api/v1/sales/'
 class SalesList(Resource):
     def get(self):
-        query_params = request.args
-        query = "SELECT Name, Platform, Global_Sales as Sales\
-        FROM games "
-
-        limit = query_params.get('limit')
-        conn = sqlite3.connect('game_data.db')
-        conn.row_factory = dict_factory
-        cur = conn.cursor()
-
-        query += "ORDER BY Name "
-        if limit:
-            limit = convert_limit(limit)
-            query += "LIMIT ?;"
-        else:
-            query += "LIMIT 100;"
-            
-        result = cur.execute(query, (limit,)).fetchall()
-        arr = []
-        
+        args = request.args
         n = 0
-        while n < len(result):
-            dict_1 = {}
-            plat_sale_arr = []
-            if not result[n]['Name']:
+        lst = []
+        # construct the sales JSON format
+        while n < len(db):
+            d = {}
+            sales_lst = []
+
+            if not db[n]['Name']:
                 n += 1
                 continue
-            dict_1['game'] = result[n]['Name']
-            while result[n]['Name'] == dict_1['game']:
-                del result[n]['Name']
-                plat_sale_arr.append(result[n])
+
+            d['game'] = db[n]['Name']
+
+            # construct the inner list
+            while db[n]['Name'] == d['game']:
+                sales_lst.append({'platform': db[n]['Platform'],
+                                  'global_sales': db[n]['Global_Sales']})
                 n += 1
-                if n == len(result):
+                if n == len(db):
                     break
-            dict_1['sales'] = list({v['Platform']: v for v in plat_sale_arr}.values())
-            arr.append(dict_1)
 
-        return jsonify(arr)
+            d['sales'] = sales_lst
+            lst.append(d)
+        if args.get('limit'):
+            limit = int(args.get('limit'))
+            return lst[:limit]
+        return lst[:20]
 
 
+# Generate endpoints to search for specific game with its sales across different platforms
+# endpoint '/api/v1/sales/search/'
 class SalesByName(Resource):
     def get(self):
-        query_params = request.args
-        query = "SELECT Name, Platform, Global_Sales as Sales\
-        FROM games WHERE Name = ?;"
-        name = query_params.get('name')
+        args = request.args
+        name = args.get('name')
 
-        conn = sqlite3.connect('game_data.db')
-        conn.row_factory = dict_factory
-        cur = conn.cursor()
-
-        result = cur.execute(query, (name,)).fetchall()
-        if len(result) == 0:
-            abort(404)
-
-        d = {'game': name, 'sales': []}
+        result = [db[n] for n in range(len(db)) if db[n]['Name'] == name]
+        d = {'game': name}
+        sales_lst = []
         for item in result:
-            del item['Name']
-            d['sales'].append(item)
+            sales_lst.append({'platform': item['Platform'],
+                              'global_sales': item['Global_Sales']})
+        d['sales'] = sales_lst
+        if not name:
+            abort(404)
+        return d
 
-        return jsonify(d)
 
-
-api.add_resource(GameList, '/api/v1/')
-api.add_resource(GameByName, '/api/v1/games')
-api.add_resource(SalesList, '/api/v1/sales')
+api.add_resource(GameList, '/api/v1/games/')
+api.add_resource(GameByParams, '/api/v1/games/<int:game_id>')
+api.add_resource(SalesList, '/api/v1/sales/')
 api.add_resource(SalesByName, '/api/v1/sales/search')
 
 if __name__ == '__main__':
