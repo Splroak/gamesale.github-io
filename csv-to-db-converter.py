b1a99a3d6674aa8f3a7836c19e2982a607e4f2ae
Splroak <hoangduyanh30@gmail.com>
2020-03-23 17:55:31 +0200

    cleaning debris

--- csv-to-db-converter.py
+++ csv-to-db-converter.py
@@ -10,6 +10,3 @@ import sqlite3
 game_db = pd.read_csv('game_data_2.csv')
 conn = sqlite3.connect("game_data.db")
 game_db.to_sql('games', conn, if_exists='append', index=False)
-
-c = conn.cursor()
-c.execute
